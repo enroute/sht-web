596d91accd5fd752ea921d28d29dd5b9284571ca
Mario Li <mario.li@tcl.com>
2017-10-19 11:34:18 +0000

    Use touchstart/touchend to detect scroll

--- static/js/common.js
+++ static/js/common.js
@@ -208,3 +208,51 @@ function formatTemplate(dta, tmpl){
 $(window).on("load", function(){
     console.log("" + $(this).scrollTop() + ", " + $(this).height() + ", " + $(document).height());
 });
+
+function swipeDetect(el, callback){
+    var touchsurface = el;
+    var swipedir, startX, startY, distX, distY,
+    threshold = 150,
+    restraint = 100,
+    allowedTime = 300, // maximum time allowed to travel that distance
+    elapsedTime,
+    startTime,
+
+    handleswipe = callback || function(swipedir){}
+  
+    touchsurface.addEventListener('touchstart', function(e){
+        var touchobj = e.changedTouches[0]
+        swipedir = 'none'
+        dist = 0
+        startX = touchobj.pageX
+        startY = touchobj.pageY
+        startTime = new Date().getTime() // record time when finger first makes contact with surface
+        //e.preventDefault()
+    }, false)
+  
+    touchsurface.addEventListener('touchmove', function(e){
+        //e.preventDefault() // prevent scrolling when inside DIV
+    }, false)
+  
+    touchsurface.addEventListener('touchend', function(e){
+        var touchobj = e.changedTouches[0]
+        distX = touchobj.pageX - startX // get horizontal dist traveled by finger while in contact with surface
+        distY = touchobj.pageY - startY // get vertical dist traveled by finger while in contact with surface
+        elapsedTime = new Date().getTime() - startTime // get time elapsed
+        //if (elapsedTime <= allowedTime){ // first condition for swipe met
+        if (1){
+            //if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){ // 2nd condition for horizontal swipe met
+            if (Math.abs(distX) >= Math.abs(distY)){
+                swipedir = (distX < 0)? 'left' : 'right' // if dist traveled is negative, it indicates left swipe
+            }
+            //else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint){ // 2nd condition for vertical swipe met
+            else {
+                swipedir = (distY < 0)? 'up' : 'down' // if dist traveled is negative, it indicates up swipe
+            }
+        }
+        handleswipe(swipedir)
+        var a = {"distX":distX, "distY":distY, "elapsedTime":elapsedTime, "allowedTime":allowedTime};
+        console.log(a);
+        //e.preventDefault()
+    }, false)
+}
